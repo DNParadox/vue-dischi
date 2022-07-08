db11727c8b7883c5b55bb830358eb6de92382ae8
DNParadox <m96francesco@gmail.com>
2022-07-08 13:51:02 +0200

    First Push

---
+++ src/components/DiskHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        test
+    </header>
+</template>
+
+
+<script>
+export default {
+name: 'DiskHeader'
+}
+</script>
+
+
+<style>
+header {
+    height: 50px;
+    background-color: #2e3a46;
+}
+</style>
